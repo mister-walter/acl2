451caa6872a0f76ef94e1db291004d4671b38c34
Drew Walter <mist3rwalter@gmail.com>
2023-10-04 16:27:04 -0400

    Add missing xslt

--- books/xdoc/fancy/xslt.js
+++ books/xdoc/fancy/xslt.js
@@ -38,13 +38,15 @@
 //   renderText(String of XDOC XML markup) -> String of Plain Text
 //   renderHtml(String of XDOC XML markup) -> HTML DOM Object
 
+import { xslt_base64 } from './render.js';
+
 // Warning: Keep this in sync with *entity-strings* and
 // *entitytok-as-plaintext-fal* in ../parse-xml.lisp,
 // *xml-entity-stuff* in ../prepare-topic.lisp, and
 // (defxdoc entities ...) in topics.lisp.
 function wrapXdocFragment(str)
 {
-    var wrap = "<!DOCTYPE xdoc [";
+    let wrap = "<!DOCTYPE xdoc [";
     wrap += "<!ENTITY ndash \"&#8211;\">";
     wrap += "<!ENTITY mdash \"&#8212;\">";
     wrap += "<!ENTITY larr \"&#8592;\">";
@@ -126,159 +128,164 @@ function wrapXdocFragment(str)
     return wrap;
 }
 
-var xslt_impl = {};
-
 // create the nodeType constants if the Node object is not defined
-if (!window.Node)
-{
-    var Node = {
-	ELEMENT_NODE                :  1,
-	ATTRIBUTE_NODE              :  2,
-	TEXT_NODE                   :  3,
-	CDATA_SECTION_NODE          :  4,
-	ENTITY_REFERENCE_NODE       :  5,
-	ENTITY_NODE                 :  6,
-	PROCESSING_INSTRUCTION_NODE :  7,
-	COMMENT_NODE                :  8,
-	DOCUMENT_NODE               :  9,
-	DOCUMENT_TYPE_NODE          : 10,
-	DOCUMENT_FRAGMENT_NODE      : 11,
-	NOTATION_NODE               : 12
-    };
-}
+// if (!window.Node)
+// {
+//     var Node = {
+// 	ELEMENT_NODE                :  1,
+// 	ATTRIBUTE_NODE              :  2,
+// 	TEXT_NODE                   :  3,
+// 	CDATA_SECTION_NODE          :  4,
+// 	ENTITY_REFERENCE_NODE       :  5,
+// 	ENTITY_NODE                 :  6,
+// 	PROCESSING_INSTRUCTION_NODE :  7,
+// 	COMMENT_NODE                :  8,
+// 	DOCUMENT_NODE               :  9,
+// 	DOCUMENT_TYPE_NODE          : 10,
+// 	DOCUMENT_FRAGMENT_NODE      : 11,
+// 	NOTATION_NODE               : 12
+//     };
+// }
 
 function unfuglifyLegacyQuotes (node)
 {
     switch(node.nodeType) {
-	case Node.ELEMENT_NODE:
-	    // Don't fix up quotes when there is code involved.
-	    // Unfortunately we have to keep this in sync with render.js
-	    var tag = node.tagName.toLowerCase();
-	    if (tag == "pre" || tag == "tt" || tag == "code")
-		return;
+		case Node.ELEMENT_NODE:
+			// Don't fix up quotes when there is code involved.
+			// Unfortunately we have to keep this in sync with render.js
+			const tag = node.tagName.toLowerCase();
+			if (tag == "pre" || tag == "tt" || tag == "code")
+				return;
 
-	    var cl = node.getAttribute("class");
-	    if (cl == "v" || cl == "tt" || cl == "mathfrag" || cl == "mathblock")
-		return;
-	break;
+			const cl = node.getAttribute("class");
+			if (cl == "v" || cl == "tt" || cl == "mathfrag" || cl == "mathblock")
+				return;
+		break;
 
-	case Node.TEXT_NODE:
-	    var text = node.nodeValue;
- 	    // Apparently we can't use html entities here like &ldquo; or you end
-	    // up with something like &amp;ldquo; in your document.  Fortunately the
-	    // unicode escapes work.  Thanks stackoverflow.
-  	    text = text.replace(/``/g, '\u201c')   // ldquo, smart `` quote
-	               .replace(/''/g, '\u201d')   // rdquo, smart '' quote
-                       .replace(/`/g,  '\u2018')   // lsquo, smart ` quote
-	               .replace(/'/g,  '\u2019');  // rsquo, smart ' quote
-	    node.nodeValue = text;
-	break;
+		case Node.TEXT_NODE:
+			let text = node.nodeValue;
+			// Apparently we can't use html entities here like &ldquo; or you end
+			// up with something like &amp;ldquo; in your document.  Fortunately the
+			// unicode escapes work.  Thanks stackoverflow.
+			text = text.replace(/``/g, '\u201c')   // ldquo, smart `` quote
+					.replace(/''/g, '\u201d')   // rdquo, smart '' quote
+					.replace(/`/g,  '\u2018')   // lsquo, smart ` quote
+					.replace(/'/g,  '\u2019');  // rsquo, smart ' quote
+			node.nodeValue = text;
+		break;
     }
 
-    for(var i = 0; i < node.childNodes.length; ++i)
-    {
-	var child = node.childNodes[i];
-	unfuglifyLegacyQuotes(child);
+    for(let i = 0; i < node.childNodes.length; ++i) {
+		const child = node.childNodes[i];
+		unfuglifyLegacyQuotes(child);
     }
 }
+
+/**
+ * @callback RenderText
+ * @param {string} str
+ * @returns {string}
+ */
+/**
+ * @type {RenderText}
+ */
+let renderText;
+/**
+ * @callback RenderHtml
+ * @param {string} str
+ * @returns {DocumentFragment}
+ */
+/**
+ * @type {RenderHtml}
+ */
+let renderHtml;
 
 function xsltInit() {
-
+	const xslt_plain = atob(xslt_base64);
     if (window.ActiveXObject // detects IE <= version 11
         || "ActiveXObject" in window // detects IE version 11
-       )
-    {
-	var xslt_plain = $.base64.decode(xslt_base64);
+       ) {
+		const xsltdom = new ActiveXObject("Msxml2.DOMDocument.6.0");
+		xsltdom.validateOnParse = true;
+		xsltdom.async = false;
+		xsltdom.loadXML(xslt_plain);
 
-	var xsltdom = new ActiveXObject("Msxml2.DOMDocument.6.0");
-	xsltdom.validateOnParse = true;
-	xsltdom.async = false;
-	xsltdom.loadXML(xslt_plain);
-	xslt_impl["proc"] = xsltdom;
+		const xmldom  = new ActiveXObject("Msxml2.DOMDocument.6.0");
+		xmldom.validateOnParse = false;
+		xmldom.async = false;
+		xmldom.setProperty("ProhibitDTD", false);
 
-	var xmldom  = new ActiveXObject("Msxml2.DOMDocument.6.0");
-	xmldom.validateOnParse = false;
-	xmldom.async = false;
-	xmldom.setProperty("ProhibitDTD", false);
+		renderText = function(str) {
+			xmldom.loadXML(wrapXdocFragment(str));
+			if (xmldom.parseError.errorCode != 0) {
+			const  myErr = xmldom.parseError;
+			try {
+				console.log("IE XML Error: " + myErr.reason);
+			}catch(e) {}
+			return "Error: " + myErr.reason;
+			}
+			const output = xmldom.transformNode(xsltdom);
+			const outStr = $("<div>" + output + "</div>").text();
+			const ret = String(outStr)
+				.replace(/"/g, '&quot;')
+				.replace(/'/g, '&apos;');
+			return ret;
+		};
 
-	xslt_impl["renderText"] = function(str) {
-	    xmldom.loadXML(wrapXdocFragment(str));
-	    if (xmldom.parseError.errorCode != 0) {
-		var myErr = xmldom.parseError;
-		try {
-		    console.log("IE XML Error: " + myErr.reason);
-		}catch(e) {}
-		return "Error: " + myErr.reason;
-	    }
-	    var output = xmldom.transformNode(xsltdom);
-	    var str = $("<div>" + output + "</div>").text();
-	    var ret = String(str)
- 		       .replace(/"/g, '&quot;')
-		       .replace(/'/g, '&apos;');
-	    return ret;
-	};
+		renderHtml = function(str) {
+			xmldom.loadXML(wrapXdocFragment(str));
+			if (xmldom.parseError.errorCode != 0) {
+			const myErr = xmldom.parseError;
+			try {
+				console.log("IE XML Error: " + myErr.reason);
+			} catch(e) {}
+				return "Error: " + myErr.reason;
+			}
+			const output = xmldom.transformNode(xsltdom);
+			// Don't try to unfuglify quotes on IE, I run into errors
+			// in code like node.childNodes.length and it just isn't worth
+			// trying to figure out.
+			// unfuglifyLegacyQuotes(output);
+			const outStr = $("<div>" + output + "</div>");
+			return outStr;
+		};
+    } else {
+		// Initialize an XSLT processor for XDOC --> HTML conversion
+		const proc = new XSLTProcessor();
+		const xslt_dom = new DOMParser().parseFromString(xslt_plain, "text/xml");
+		// var xslt_dom = $.parseXML(xslt_plain);
+		proc.importStylesheet(xslt_dom);
 
-	xslt_impl["renderHtml"] = function(str) {
-	    xmldom.loadXML(wrapXdocFragment(str));
-	    if (xmldom.parseError.errorCode != 0) {
-		var myErr = xmldom.parseError;
-		try {
-		    console.log("IE XML Error: " + myErr.reason);
-		}catch(e) {}
-		return "Error: " + myErr.reason;
-	    }
-	    var output = xmldom.transformNode(xsltdom);
-	    // Don't try to unfuglify quotes on IE, I run into errors
-	    // in code like node.childNodes.length and it just isn't worth
-	    // trying to figure out.
-	    // unfuglifyLegacyQuotes(output);
-	    var str = $("<div>" + output + "</div>");
-	    return str;
-	};
+		renderText = function(str) {
+			const xml = new DOMParser().parseFromString(wrapXdocFragment(str), "text/xml");
+			// var xml = $.parseXML(wrapXdocFragment(str));
+			const dom = proc.transformToFragment(xml,document);
+			const outStr = $(dom).text();
+
+			// It's not clear to me whether this is good or bad.  The
+			// basic problem is that strings like "short" strings
+			// might legitimately have quotes or apostrophes in them.
+			// This is no problem if we're going to stick the
+			// renderText into a paragraph or something like that.
+			// But it *is* a problem if we're going to stick it into
+			// an attribute like a tooltip or similar.  So, just to
+			// avoid problems like that, make sure the resulting
+			// string is free of " and ' characters.
+			return String(outStr)
+			.replace(/"/g, '&quot;')
+			.replace(/'/g, '&apos;');
+		};
+
+		renderHtml = function(str) {
+			const xml = new DOMParser().parseFromString(wrapXdocFragment(str), "text/xml");
+			// var xml = $.parseXML(wrapXdocFragment(str));
+			const dom = proc.transformToFragment(xml,document);
+			unfuglifyLegacyQuotes(dom);
+			return dom;
+		};
     }
-
-    else {
-
-	// Initialize an XSLT processor for XDOC --> HTML conversion
-	xslt_impl["proc"] = new XSLTProcessor();
-	var xslt_plain = $.base64.decode(xslt_base64);
-	var xslt_dom = $.parseXML(xslt_plain);
-	xslt_impl["proc"].importStylesheet(xslt_dom);
-
-	xslt_impl["renderText"] = function(str) {
-	    var xml = $.parseXML(wrapXdocFragment(str));
-	    var dom = xslt_impl["proc"].transformToFragment(xml,document);
-	    var str = $(dom).text();
-
-	    // It's not clear to me whether this is good or bad.  The
-	    // basic problem is that strings like "short" strings
-	    // might legitimately have quotes or apostrophes in them.
-	    // This is no problem if we're going to stick the
-	    // renderText into a paragraph or something like that.
-	    // But it *is* a problem if we're going to stick it into
-	    // an attribute like a tooltip or similar.  So, just to
-	    // avoid problems like that, make sure the resulting
-	    // string is free of " and ' characters.
-	    return String(str)
-		.replace(/"/g, '&quot;')
-		.replace(/'/g, '&apos;');
-	};
-
-	xslt_impl["renderHtml"] = function(str) {
-	    var xml = $.parseXML(wrapXdocFragment(str));
-	    var dom = xslt_impl["proc"].transformToFragment(xml,document);
-	    unfuglifyLegacyQuotes(dom);
-	    return dom;
-	};
-    }
-}
-
-function renderText(str) {
-    return xslt_impl["renderText"](str);
-}
-
-function renderHtml(str) {
-    return xslt_impl["renderHtml"](str);
 }
 
 xsltInit();
+
+export { renderText, renderHtml }
